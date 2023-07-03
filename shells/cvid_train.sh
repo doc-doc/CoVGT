27cfc2200a1337c2faa1926d8093ea30cecad3bd
doc-doc <junbin@comp.nus.edu.sg>
2023-07-03 21:31:57 +0800

    initial commit

---
+++ shells/cvid_train.sh
@@ -0,0 +1,23 @@
+GPU=$1
+CUDA_VISIBLE_DEVICES=$GPU python main.py --checkpoint_dir=causalvid \
+	--dataset=causalvid \
+	--mc=5 \
+	--bnum=10 \
+	--epochs=20 \
+	--lr=0.00001 \
+	--qmax_words=0 \
+	--amax_words=38 \
+	--max_feats=32 \
+	--batch_size=64 \
+	--batch_size_val=64 \
+	--num_thread_reader=8 \
+	--mlm_prob=0 \
+	--n_layers=1 \
+	--embd_dim=512 \
+	--ff_dim=1024 \
+	--dropout=0.3 \
+	--seed=666 \
+	--cl_loss=0 \
+	--lan="RoBERTa" \
+	--save_dir='./save_models/causalvid/CoVGT/' \
+	--pretrain_path='./save_models/causalvid/CoVGT/best_model.pth'
